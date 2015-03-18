678759a25205b39287754da8ea052046dc8b6758
Dmitry Vakylenko <kremdima@mail.ru>
2015-03-18 22:17:29 +0200

    added logger provider
    q

---
+++ Storm/Storm.Config/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storm.Config
+{
+    public class Config : Dictionary<string, object>
+    {
+        public const string STORM_LOCAL_HOSTNAME = "storm.local.hostname";
+
+    }
+}
